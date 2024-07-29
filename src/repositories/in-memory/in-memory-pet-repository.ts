b49619ab0e9179e6c9cbedd403b0814dcc7fbbe4
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 12:25:21 -0300

    feat: Add method findAll

--- src/repositories/in-memory/in-memory-pet-repository.ts
+++ src/repositories/in-memory/in-memory-pet-repository.ts
@@ -2,10 +2,32 @@ import crypto from "node:crypto"
 
 import { Pet, Prisma } from "@prisma/client";
 
-import { PetRepository } from "../pet-repository";
+import { FindAllParams, PetRepository } from "../pet-repository";
+import { InMemoryOrgRepository } from "./in-memory-org-repository";
 
 export class InMemoryPetRepository implements PetRepository {
   public items: Pet[] = []
+
+  constructor(private orgsRepository: InMemoryOrgRepository) { }
+
+  async findAll(params: FindAllParams) {
+    const orgsByCity = this.orgsRepository.items.filter(
+      (org) => org.city === params.city
+    )
+
+    const pets = this.items
+      .filter((item) => orgsByCity.some((org) => org.id === item.org_id))
+      .filter((item) => (params.age ? item.age === params.age : true))
+      .filter((item) => (params.size ? item.size === params.size : true))
+      .filter((item) =>
+        params.energy_level ? item.energy_level === params.energy_level : true,
+      )
+      .filter((item) =>
+        params.environment ? item.environment === params.environment : true,
+      )
+
+    return pets
+  }
 
   async create(data: Prisma.PetUncheckedCreateInput) {
     const pet = {
