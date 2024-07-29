9cbeb494bf71cdbf58cc00873e3dcece10ae288e
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 11:18:06 -0300

    feat: create org use-case

---
+++ src/use-cases/create-org.ts
@@ -0,0 +1,70 @@
+import { Org } from "@prisma/client"
+import { hash } from "bcryptjs"
+
+import { OrgRepository } from "@/repositories/org-repository"
+import { OrgAlreadyExistsError } from "./errors/org-already-exists-error"
+
+interface CreateOrgUseCaseRequest {
+  name: string
+  author_name: string
+  email: string
+  whatsapp: string
+  password: string
+  cep: string
+  state: string
+  city: string
+  neighborhood: string
+  street: string
+  latitude: number
+  longitude: number
+}
+
+interface CreateOrgUseCaseResponse {
+  org: Org
+}
+
+export class CreateOrgUseCase {
+  constructor(private orgRepository: OrgRepository) { }
+
+  async execute({
+    name,
+    author_name,
+    email,
+    whatsapp,
+    password,
+    cep,
+    state,
+    city,
+    neighborhood,
+    street,
+    latitude,
+    longitude,
+  }: CreateOrgUseCaseRequest): Promise<CreateOrgUseCaseResponse> {
+    const orgWithSameEmail = await this.orgRepository.findByEmail(email)
+
+    if (orgWithSameEmail) {
+      throw new OrgAlreadyExistsError()
+    }
+
+    const password_hash = await hash(password, 6)
+
+    const org = await this.orgRepository.create({
+      name,
+      author_name,
+      email,
+      whatsapp,
+      password: password_hash,
+      cep,
+      state,
+      city,
+      neighborhood,
+      street,
+      latitude,
+      longitude,
+    })
+
+    return {
+      org
+    }
+  }
+}
