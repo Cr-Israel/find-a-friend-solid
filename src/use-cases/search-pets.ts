d966a2344792585045e5aaf903b0d0b979ae1b88
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 12:26:32 -0300

    feat: Search all pets by city and other informations

---
+++ src/use-cases/search-pets.ts
@@ -0,0 +1,42 @@
+import { Pet } from "@prisma/client"
+
+import { PetRepository } from "@/repositories/pet-repository"
+
+interface SearchPetsUseCaseRequest {
+  city: string
+  age?: string
+  size?: string
+  energy_level?: string
+  environment?: string
+}
+
+interface SearchPetsUseCaseResponse {
+  pets: Pet[]
+}
+
+export class SearchPetsUseCase {
+  constructor(
+    private petRepository: PetRepository
+  ) { }
+
+  async execute({
+    city,
+    age,
+    size,
+    energy_level,
+    environment
+  }: SearchPetsUseCaseRequest): Promise<SearchPetsUseCaseResponse> {
+
+    const pets = await this.petRepository.findAll({
+      city,
+      age,
+      size,
+      energy_level,
+      environment
+    })
+
+    return {
+      pets
+    }
+  }
+}
