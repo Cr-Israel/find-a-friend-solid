c788e21e6a556ccf53a31a34abeee2b3ba9328ba
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 15:38:32 -0300

    feat: fetch nearby orgs test file

---
+++ src/use-cases/fetch-nearby-orgs.spec.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from "vitest"
+
+import { FetchNearbyGymsUseCase } from "./fetch-nearby-orgs"
+import { InMemoryOrgRepository } from "@/repositories/in-memory/in-memory-org-repository"
+
+let orgRepository: InMemoryOrgRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('Fetch Neaby Orgs', () => {
+  beforeEach(() => {
+    orgRepository = new InMemoryOrgRepository()
+    sut = new FetchNearbyGymsUseCase(orgRepository)
+  })
+
+  it('should be able to fetch neaby orgs', async () => {
+    const org = await orgRepository.create({
+      name: 'Bonitos',
+      author_name: 'Carlos',
+      email: 'org@example.com',
+      whatsapp: '2199999-8888',
+      password: '123456',
+      cep: '21865-354',
+      state: 'RJ',
+      city: 'Rio de Janeiro',
+      neighborhood: '',
+      street: 'Rua dos Bonitos, 122',
+      latitude: -22.872064,
+      longitude: -43.3160192,
+    })
+
+    const nearbyOrgs = await sut.execute({
+      userLatitude: org.latitude.toNumber(),
+      userLongitude: org.longitude.toNumber(),
+    })
+
+    expect(nearbyOrgs.orgs).toEqual([org])
+    expect(nearbyOrgs.orgs[0].email).toEqual('org@example.com')
+  })
+})
