625ac6bb2ba607336e41f1c6a937229b03f90c27
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 12:25:04 -0300

    feat: Add method findAll and create an interface FindAllParams

--- src/repositories/pet-repository.ts
+++ src/repositories/pet-repository.ts
@@ -1,5 +1,14 @@
 import { Pet, Prisma } from "@prisma/client";
 
+export interface FindAllParams {
+  city: string
+  age?: string
+  size?: string
+  energy_level?: string
+  environment?: string
+}
+
 export interface PetRepository {
+  findAll(params: FindAllParams): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
