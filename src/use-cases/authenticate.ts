8c5c037282a1143813108804768fd8756026eaa4
Cr-Israel <carlosisrael08@hotmail.com>
2024-07-29 11:18:38 -0300

    feat: authenticate org use-case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,39 @@
+import { Org } from "@prisma/client"
+import { compare } from "bcryptjs"
+
+import { OrgRepository } from "@/repositories/org-repository"
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error"
+
+interface AuthenticateOrgUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateOrgUseCaseResponse {
+  org: Org
+}
+
+export class AuthenticateOrgUseCase {
+  constructor(private orgRepository: OrgRepository) { }
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateOrgUseCaseRequest): Promise<AuthenticateOrgUseCaseResponse> {
+    const org = await this.orgRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, org.password)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+    
+    return {
+      org
+    }
+  }
+}
