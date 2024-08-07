e547c164625fbe8aea5358e1ec409e12cbfec91f
Cr-Israel <carlosisrael08@hotmail.com>
2024-08-07 16:12:26 -0300

    feat: main file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { ZodError } from "zod";
 
 import { env } from "./env";
+import { orgsRoutes } from "./http/routes/orgs/orgs-routes";
+import { petsRoutes } from "./http/routes/pets/pets-routes";
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false // Não é um Cookie assinado
+  },
+  sign: {
+    expiresIn: '10m' // minutes
+  }
+})
+
+app.register(fastifyCookie)
+
+app.register(orgsRoutes)
+app.register(petsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
